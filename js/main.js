7d40b7de41eec7963d0d8d524c4fedcf2e459b19
MaksimchikKsenia <kseniamaksimcik3@gmail.com>
2023-09-11 21:12:27 +0300

    first upd

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
+  var instances_subnav = M.Sidenav.init(elems);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var photo = document.querySelectorAll(".materialboxed");
+  var instances_photo = M.Materialbox.init(photo);
+});
